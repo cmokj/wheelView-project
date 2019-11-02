ea91ca25c0dd99b9098a07c13394948e0c8e6852
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-11-02 17:37:45 +0800

    将 button 组件修改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .g-button {
+        padding: 0 1em;
+        height: var(--button-height);
+        font-size: var(--font-size);
+        background-color: var(--button-bg);
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        color: var(--color);
+        &:hover {
+            border: 1px solid var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
