86e6495731790c23a95f8b45d4dbc188013fc91e
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-11-03 12:53:57 +0800

    添加 icon 并设置左右属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,24 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" v-bind:class="`icon-${iconPosition}`">
+        <svg class="icon" aria-hidden="true" v-if="icon">
+            <use v-bind:xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left'
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
@@ -15,6 +30,10 @@
         border: 1px solid var(--border-color);
         border-radius: var(--border-radius);
         color: var(--color);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
             border: 1px solid var(--border-color-hover);
         }
@@ -23,6 +42,27 @@
         }
         &:focus {
             outline: none;
+        }
+        > svg {
+            width: 1em;
+            height: 1em;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-left: .1em;
+                margin-right: 0;
+            }
+            > .content {
+                order: 1;
+            }
         }
     }
 </style>
